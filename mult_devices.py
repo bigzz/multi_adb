a57033708ff5a7cbb0915e4076fdcd2ddf02b113
Wenjun.Zhang <zhangwenjun1@meizu.com>
2015-07-13 22:18:32 +0800

    Add callback function framework.

--- mult_devices.py
+++ mult_devices.py
@@ -6,6 +6,16 @@ def get_devices(adb):
     devices = adb.get_devices()
     return devices[1:]
 
+def push_bin(adb, device_name):
+    local = 'iozone'
+    remote = '/data'
+    adb.push_local_file(local, remote)
+
+def do_work(adb, funcs,argv):
+    for func in funcs:
+        if func:
+            func(adb, argv)
+
 def main():
     # creates the ADB object
     adb = ADB()
